0e3e7bde4031288692abc0cc15b2b4b8c0f59b8a
Ben Hanna <benpaulhanna@gmail.com>
2020-12-12 10:28:46 -0500

    Added test data.

--- main.scm
+++ main.scm
@@ -3,9 +3,13 @@
 (define screen-w 320)
 (define screen-h 240)
 
+(define tileset "tileset.bmp")
+
 (if (zero? (init-system "Hello" screen-w screen-h))
 	(begin
+		(load-tileset tileset)
 		(clear-screen)
+		(put-tile 0 0 0 0)
 		(refresh-screen)
 		(sleep 2000)
         (stop-system)))
