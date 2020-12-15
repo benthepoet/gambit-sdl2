32c6806e3f061a2e0e7c2e04d80b7f965f388060
Ben Hanna <benpaulhanna@gmail.com>
2020-12-14 22:12:49 -0500

    Implemented loop timer.

--- main.scm
+++ main.scm
@@ -1,9 +1,11 @@
 (include "system.scm")
 (include "util.scm")
 
-(define screen-w 320)
-(define screen-h 240)
-(define screen-scale 2)
+(define +screen-w+ 320)
+(define +screen-h+ 240)
+(define +screen-scale+ 2)
+(define +ticks-per-second+ 30)
+(define +skip-ticks+ (truncate (/ 1000 +ticks-per-second+)))
 
 (define tileset "../assets/tileset.bmp")
 
@@ -19,19 +21,32 @@
                             (set! i (+ i 8))) 
                         row)
                     (set! i x)
-                    (set! j (+ j 8))
-                    )
+                    (set! j (+ j 8)))
                 rows
             ))))
 
-(if (zero? (init-system "Demo" screen-w screen-h screen-scale))
+(if (zero? (init-system "Demo" +screen-w+ +screen-h+ +screen-scale+))
     (begin
         (load-tileset tileset)
-        (let loop ()
-            (clear-screen)
-            (put-sprite 64 64 '#(#(0 1 16) #(17 2 18)))
-            (refresh-screen)
-            (sleep 1000)
-            (loop)
-        )
+        (let ((run #t) 
+              (next-tick 0))
+            (let main-loop ()
+                (set! next-tick (+ (get-ticks) +skip-ticks+))
+
+                (clear-screen)
+                (put-sprite 64 64 '#(#(0 1 16) #(17 2 18)))
+                (refresh-screen)
+
+                (let event-loop ()
+                    (if (poll-event) 
+                        (begin 
+                            (case (event-type) 
+                                ((#x100) (set! run #f)))
+                            (event-loop))))
+
+                (let ((sleep-ticks (- next-tick (get-ticks))))
+                    (if (> sleep-ticks 0)
+                        (sleep sleep-ticks)))
+
+                (if run (main-loop))))
         (stop-system)))
