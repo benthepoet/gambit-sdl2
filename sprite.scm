b594179ed47513f591817b940d13a00585dbd405
Ben Hanna <ben.hanna@vpdcs.com>
2020-12-31 15:55:00 -0500

    Fixed indentation.

--- sprite.scm
+++ sprite.scm
@@ -1,15 +1,14 @@
 (define put-sprite
-    (lambda
-        (x y rows)
-        (let ((i x) (j y))
-            (vec-each
-                (lambda (row)
-                    (vec-each
-                        (lambda (tile)
-                            (put-tile tile i j)
-                            (set! i (+ i 8))) 
-                        row)
-                    (set! i x)
-                    (set! j (+ j 8)))
-                rows
-            ))))
+  (lambda
+      (x y rows)
+    (let ((i x) (j y))
+      (vec-each
+       (lambda (row)
+         (vec-each
+          (lambda (tile)
+            (put-tile tile i j)
+            (set! i (+ i 8))) 
+          row)
+         (set! i x)
+         (set! j (+ j 8)))
+       rows))))
